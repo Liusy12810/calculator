c5b1a539d6a6a6eaf5ba9ab93068d7e83e0569d6
Toc320A <liusy12810@outlook.com>
2022-01-15 16:42:47 +0800

    separated the function set_Operator() to three functions

--- calculator1/include/expression.cpp
+++ calculator1/include/expression.cpp
@@ -7,7 +7,7 @@ namespace expr {
 // class method definitions for Expression
 	Expression::Expression() :
 		_exp("") {
-		_Op = nullptr;
+		_op = nullptr;
 	};
 
 	Expression::Expression(std::string Str_exp) :
@@ -16,33 +16,54 @@ namespace expr {
 	};
 
 	Expression::~Expression() {
-		delete _Op;
+		delete _op;
 	};
 
-	void Expression::set_Operator(const std::string & Str_exp) {
+	void Expression::set_Operator() {
 
-		int left = 0, right = 0;
+		int i;
 		int op_id = -1;
-		int len = Str_exp.length();
+		int left = 0, right = 0;
+		int len = _exp.length();
+		std::string sub_Str_left  = "0";
+		std::string sub_Str_right = "0";
 
-		for (int i = 0; i < len; i++) {
-			if (!isdigit(Str_exp[i])) {
-				if (op::isop(Str_exp[i]) > 0) {
-					op_id = op::isop(Str_exp[i]);
-				}
-			}
+		i = find_Operator();
+
+		if (i > 0 && i < len) {
+			sub_Str_left  = _exp.substr(0, i);
+			sub_Str_right = _exp.substr(i + 1);
+			left  = std::stod(sub_Str_left);
+			right = std::stod(sub_Str_right);
 		}
+
+		set_op(op_id, left, right);
+	}
+
+	void Expression::set_op(int op_id, double left, double right) {
 		switch (op_id) {
-		case 1: _Op = new op::OpPlus   (left, right);
+		case 1: _op = new op::OpPlus(left, right);
 			break;
-		case 2: _Op = new op::OpMinus  (left, right);
+		case 2: _op = new op::OpMinus(left, right);
 			break;
-		case 3: _Op = new op::OpProduct(left, right);
+		case 3: _op = new op::OpProduct(left, right);
 			break;
-		case 4: _Op = new op::OpDivide (left, right);
+		case 4: _op = new op::OpDivide(left, right);
 			break;
 		default: throw std::exception("Invalid expression: No such operator");
 			break;
 		}
 	}
+
+	int Expression::find_Operator() const{
+		int i;
+		int len = _exp.length();
+		for (i = 0; i < len; i++) {
+			if (op::isop(i) > 0)
+			{
+				break;
+			}
+		}
+		return i;
+	};
 }
